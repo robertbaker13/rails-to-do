704250f13c4ffc3841862c21976fa2eb5efd25f1
Robert Baker <robertbaker13@gmail.com>
2015-08-25 10:28:51 -0700

    add more seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@ bob = User.create(username: "bob", email: "bob@bob.com", password: "123456")
 bob1 = Task.create(user: bob, content: "walk the dog", completed: false)
 bob2 = Task.create(user: bob, content: "wash the dog", completed: false)
 bob3 = Task.create(user: bob, content: "feed the dog", completed: false)
+bob4 = Task.create(user: bob, content: "do the dishes", completed: false)
+
+
+betty = User.create(username: "betty", email: "betty@betty.com", password: "123456")
+betty1 = Task.create(user: betty, content: "feed the cat", completed: false)
+betty2 = Task.create(user: betty, content: "change the litterbox", completed: false)
+betty3 = Task.create(user: betty, content: "do the laundry", completed: false)
